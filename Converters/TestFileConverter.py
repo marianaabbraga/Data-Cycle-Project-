23ae1279843b876f22d521164ff7ab370ba4e4f9
Matthias <gaillardmatthias1.9991@gmail.com>
2026-03-08 17:12:51 +0100

    Modularize bronze flow into functions

--- Converters/TestFileConverter.py
+++ Converters/TestFileConverter.py
@@ -1,3 +1,3 @@
 from fileConverter import convert_file
 
-convert_file("Tests\AAPL\AAPL_prices_2026-03-05_19-35-44.parquet", "csv")
+convert_file("Tests/AAPL/AAPL_prices_2026-03-08_17-02-15.parquet", "csv")
